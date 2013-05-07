54757566436df10877b8ffe745e513d59c069be4
Vishva <vishvazaccess@gmail.com>
2013-05-08 01:56:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Godzilla::Application.config.secret_token = 'd4d3e03aff7e91450e9fb4c47525cbc83812b7b7bb505480d9f54461c3eaa5e36d20c7e3f15e9e575553ffdcf083348b758bb8d8a70895ccba6a4db764d9235d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Godzilla::Application.config.session_store :cookie_store, key: '_Godzilla_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Godzilla::Application.config.session_store :active_record_store
